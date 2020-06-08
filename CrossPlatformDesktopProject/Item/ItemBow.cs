4d81757b1dc7a716ce379709e96afb96b7175011
isabelsmithc <isabelsmithc@gmail.com>
2020-06-08 18:40:41 -0400

    * ItemBow.cs: * ItemBomb.cs: * ItemClock.cs: * ItemFairy.cs: * ItemCompass.cs: * ItemBlueRing.cs: * ItemBlueCandle.cs: * ItemBluePotion.cs: * UseItem1Command.cs: * UseItem2Command.cs: * UseItem3Command.cs: * KeyboardController.cs: * Aquamentus.cs:
    * Sprint_2.csproj: added use item link animation, no item though

--- CrossPlatformDesktopProject/Item/ItemBow.cs
+++ CrossPlatformDesktopProject/Item/ItemBow.cs
@@ -38,7 +38,7 @@ namespace Sprint2.Item
 
         public void Use()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
     }
 }
