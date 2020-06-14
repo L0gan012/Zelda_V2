d775a50b8d11a6c295fb6e9280901eb32c1e6041
Logan Sauer <logan.sauer.012@gmail.com>
2020-06-14 14:11:49 -0400

    Got rid of comments in game

--- CrossPlatformDesktopProject/Game1.cs
+++ CrossPlatformDesktopProject/Game1.cs
@@ -14,7 +14,6 @@ namespace Sprint2
 
     public class Game1 : Game
     {
-        //Instance variables
         private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
 
@@ -53,122 +52,77 @@ namespace Sprint2
             set { link = value; }
         }
       
-        //Game constructor
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
 
-        //Initailizes all non-drawable content
         protected override void Initialize()
         {
-            //Make the mouse visible
             this.IsMouseVisible = true;
 
-            //Initializes controller object
             controller = new KeyboardController(this);
 
-            //Registers Commands for controls
             controller.RegisterCommand();
 
-            //Loads sprite content for link
             LinkSpriteFactory.Instance.LoadAllTextures(Content);
 
-            //Initializes items objects
             Items = new List<IItem>();
             ItemLoader = new ItemLoadAllContent(this);
 
-            //Initializes enemy and npc objects
             Enemies = new List<INPC>();
             EnemyLoader = new EnemyLoadAllContent(this);
 
-            //Initializes blocks object
             Blocks = new List<IBlock>();
             BlockLoader = new BlockLoadAllContent(this);
 
-           
-
-            //Initializes list position
             ICommand reset = new ResetCommand(this);
             reset.Execute();
             
-
             base.Initialize();
         }
 
-        //Initializes all of the drawable content
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            //Loads sprite content for items
             ItemsSpriteFactory.Instance.LoadAllTextures(Content);
-
-            //Load sprite content for projectiles
             ProjectileSpriteFactory.Instance.LoadAllTextures(Content);
-
-            //Enemy sprite content for items
             EnemySpriteFactory.Instance.LoadAllTextures(Content);
-
-            //NPC sprite content for items
             NPCSpriteFactory.Instance.LoadAllTextures(Content);
-
-            //Loads sprite content for blocks
             BlockSpriteFactory.Instance.LoadAllTextures(Content);
 
-            //Loads content for all items
             ItemLoader.LoadContent();
-
-            //Loads content for all enemies and npcs
             EnemyLoader.LoadContent();
-
-            //Loads content for all blocks
             BlockLoader.LoadContent();
             
         }
 
-        //Unloads content
         protected override void UnloadContent()
         {
 
         }
 
-        //Update
         protected override void Update(GameTime gameTime)
         {
-            //Updates contols
             controller.Update();
-            //Updates link object
             Link.Update();
-            //Updates items object
             Items[ItemListPosition].Update();
-            //Updates items object
             Enemies[EnemyListPosition].Update();
-            //Updates blocks object
             Blocks[BlockListPosition].Update();
 
             base.Update(gameTime);
          
         }
 
-        //Draw
         protected override void Draw(GameTime gameTime)
         {
-            //Set background color
             GraphicsDevice.Clear(Color.LightGray);
 
-            //Draws link
             link.Draw(spriteBatch);
-
-            //Draws Items
             Items[ItemListPosition].Draw(spriteBatch);
-
-            //Draws Enemies
             Enemies[EnemyListPosition].Draw(spriteBatch);
-
-            //Draw blocks
             Blocks[BlockListPosition].Draw(spriteBatch, Constant.BlockStartPosition);
 
             base.Draw(gameTime);
