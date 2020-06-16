52287f016faeb895d8652b1052313b736109dae4
Logan Sauer <logan.sauer.012@gmail.com>
2020-06-16 18:43:06 -0400

    fixed usablebomb

--- CrossPlatformDesktopProject/Item/UsableItem/UsableBomb.cs
+++ CrossPlatformDesktopProject/Item/UsableItem/UsableBomb.cs
@@ -12,33 +12,32 @@ namespace Sprint2.Item.UsableItem
     public class UsableBomb : AbstractItem, IUsableItem
     {
         private ILink link;
-        private ISprite sprite;
-        private Vector2 position;
 
         public UsableBomb(ILink link)
         {
             this.link = link;
-            sprite = ItemsSpriteFactory.Instance.CreateSpriteBomb();
+            Sprite = ItemsSpriteFactory.Instance.CreateSpriteBomb();
+            Color = Color.White;
         }
 
         public void UseLeft()
         {
-            position.X = link.Position.X - Constant.BombOffset;
+            Location = new Vector2(link.Position.X - Constant.BombOffset, Location.Y);
         }
 
         public void UseRight()
         {
-            position.X = link.Position.X + Constant.BombOffset;
+            Location = new Vector2(link.Position.X + Constant.BombOffset, Location.Y);
         }
 
         public void UseUp()
         {
-            position.Y = link.Position.Y - Constant.BombOffset;
+            Location = new Vector2(Location.X, link.Position.Y - Constant.BombOffset);
         }
 
         public void UseDown()
         {
-            position.Y = link.Position.Y + Constant.BombOffset;
+            Location = new Vector2(Location.X, link.Position.Y + Constant.BombOffset);
         }
     }
 }
