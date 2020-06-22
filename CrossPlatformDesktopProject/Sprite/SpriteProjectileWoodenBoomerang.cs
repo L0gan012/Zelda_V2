ea08052bdfb8a27dae221f64ff8e028b41cff4fe
Logan Sauer <logan.sauer.012@gmail.com>
2020-06-21 21:16:56 -0400

    Corrected boomerang sprites

--- CrossPlatformDesktopProject/Sprite/SpriteProjectileWoodenBoomerang.cs
+++ CrossPlatformDesktopProject/Sprite/SpriteProjectileWoodenBoomerang.cs
@@ -10,7 +10,7 @@ namespace Sprint2.Sprite
         {
             this.Texture = texture;
             Rows = 1;
-            Columns = 4;
+            Columns = 8;
             CurrentFrame = 0;
             CurrentTick = 0;
             TotalFrames = Rows * Columns;
