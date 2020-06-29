3192321d2c5e8223482f694f98d5cfb1b788b1c7
Mitel Yu <yulongfei0314@gmail.com>
2020-06-29 15:57:42 -0400

    Changes in block collision handler

--- CrossPlatformDesktopProject/Collision/Block Handlers/BlockBlockCollisionHandler.cs
+++ CrossPlatformDesktopProject/Collision/Block Handlers/BlockBlockCollisionHandler.cs
@@ -1,6 +1,60 @@
-﻿namespace Sprint2
+﻿using Microsoft.Xna.Framework;
+using System.Diagnostics.Contracts;
+
+namespace Sprint2
 {
-    class BlockBlockCollisionHandler
+    public static class BlockBlockCollisionHandler
     {
+        public static void HandleCollision(IBlock blockOne, IBlock blockTwo, Enumerations.CollisionSide collision)
+        {
+            Contract.Requires(blockOne != null);
+            Contract.Requires(blockTwo != null);
+
+            if (blockOne.IsMovable)
+            {
+                blockOne.Knockback = true;
+                blockOne.CollisionLocation = blockOne.Position;
+
+                switch (collision)
+                {
+
+                    case Enumerations.CollisionSide.Right:
+                        RightCollision(blockOne);
+                        break;
+                    case Enumerations.CollisionSide.Left:
+                        LeftCollision(blockOne);
+                        break;
+                    case Enumerations.CollisionSide.Top:
+                        UpCollision(blockOne);
+                        break;
+                    case Enumerations.CollisionSide.Bottom:
+                        DownCollision(blockOne);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            blockTwo.Knockback = false;
+        }
+
+        private static void RightCollision(IBlock block)
+        {
+            block.Velocity = Vector2.UnitX * Constant.BlockMovementSpeed;
+        }
+
+        private static void LeftCollision(IBlock block)
+        {
+            block.Velocity = -Vector2.UnitX * Constant.BlockMovementSpeed;
+        }
+
+        private static void UpCollision(IBlock block)
+        {
+            block.Velocity = -Vector2.UnitY * Constant.BlockMovementSpeed;
+        }
+
+        private static void DownCollision(IBlock block)
+        {
+            block.Velocity = Vector2.UnitY * Constant.BlockMovementSpeed;
+        }
     }
 }
