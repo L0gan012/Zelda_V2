34aa71403f3d0d668b65337dd00c38938d4effb7
isabelsmithc <isabelsmithc@gmail.com>
2020-06-29 21:12:43 -0400

    gave gameObjectType

--- CrossPlatformDesktopProject/Enemy_NPC/Old man/OldMan.cs
+++ CrossPlatformDesktopProject/Enemy_NPC/Old man/OldMan.cs
@@ -8,8 +8,8 @@ namespace Sprint2
 
         public override Enumerations.GameObjectType GameObjectType
         {
-            get => throw new NotImplementedException();
-            set => throw new NotImplementedException();
+            get => Enumerations.GameObjectType.EnemyGround;
+            set => _ = Enumerations.GameObjectType.EnemyGround;
         }
 
         public OldMan()
