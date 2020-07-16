b15fba5c1198720c53646cebc49e668465089987
isabelsmithc <isabelsmithc@gmail.com>
2020-07-16 19:53:49 -0400

    added health bar interface

---
+++ CrossPlatformDesktopProject/HUD/HealthBar/IHealthBar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Sprint2
+{
+    public interface IHealthBar
+    {
+        void Update();
+
+        void Draw(SpriteBatch spriteBatch);
+    }
+}
