800e8578d929ae6aa671372ebe147b9d465f0e18
Barry Birschbach <birschbach.3@osu.edu>
2020-07-15 23:55:12 -0400

    Added SpriteEffectFactory

---
+++ CrossPlatformDesktopProject/Factory/SpriteEffectSpriteFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Sprint2.Sprite;
+using System.Collections.Generic;
+
+namespace Sprint2
+{
+	public class SpriteEffectSpriteFactory
+	{
+		private Dictionary<string, Texture2D> spriteEffectSpriteContent;
+		public static SpriteEffectSpriteFactory Instance { get; } = new SpriteEffectSpriteFactory();
+
+		private SpriteEffectSpriteFactory()
+		{
+
+		}
+
+		public void LoadAllTextures(ContentManager content)
+		{
+			spriteEffectSpriteContent = ImportContent.LoadListContent<Texture2D>(content, "TextureSheets/SpriteEffectTextures");
+		}
+
+
+		public ISprite CreateSpriteEnemyDeadEffectWhite()
+		{
+			return new SpriteEffectEnemyDead(spriteEffectSpriteContent["DeadEnemyWhite"]);
+		}
+
+	}
+}
