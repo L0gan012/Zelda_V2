98ae8661f7ead6aaf309d5805362e1a05ec12f81
Terryl Rumph <trumph15bx@yahoo.com>
2020-07-21 12:02:01 -0400

    Updating more of the game selection

--- CrossPlatformDesktopProject/Command/GameSelectionCommand.cs
+++ CrossPlatformDesktopProject/Command/GameSelectionCommand.cs
@@ -1,29 +1,37 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Collections.Generic;
 
 namespace Sprint2
 {
     class GameSelectionCommand : ICommand
     {
+        private List<Vector2> selectionPosition;
+        private int listCounter;
         public GameSelectionCommand()
         {
-
+            selectionPosition = new List<Vector2>();
+            selectionPosition.Add(new Vector2(540, 300));
+            selectionPosition.Add(new Vector2(600, 190));
+            listCounter = 0;
         }
 
         public void Execute()
         {
             
             //Starts Classic Game
-            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            if (Keyboard.GetState().IsKeyDown(Keys.Space) && StateGameTypeSelection.vector.X == 600)
             {
               Game1.Instance.State = new StateInLevel();
             }
 
             //Changes selection for game wanted
             if (Keyboard.GetState().IsKeyDown(Keys.Tab))
-            {               
-                StateGameTypeSelection.vector = new Vector2(540, 300);
+            {
+                if(listCounter == selectionPosition.Count) {listCounter = 0;}
+                StateGameTypeSelection.vector = selectionPosition[listCounter];
+                listCounter++;
             }
 
 
