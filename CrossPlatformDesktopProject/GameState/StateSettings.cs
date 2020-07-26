dbae93ed293e827dd5bd8b6361525b29fc226a4e
isabelsmithc <isabelsmithc@gmail.com>
2020-07-25 20:49:51 -0400

    added settings files that didn't get included

---
+++ CrossPlatformDesktopProject/GameState/StateSettings.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace Sprint2
+{
+    public class StateSettings : IGameState
+    {
+        public Game1 Game { get; set; }
+
+        public ISettingsPage SettingsPage { get; set; }
+        public ISettingsPage PreviousPage { get; set; }
+
+        private SpriteFont font;
+
+        public StateSettings()
+        {
+            Game = Game1.Instance;
+            SettingsPage = new StartPage();
+            font = Game1.Instance.Content.Load<SpriteFont>("Fonts/Font");
+        }
+
+        public void Update()
+        {
+            foreach (IController controller in Game.Controllers)
+            {
+                controller.Update();
+            }
+            SettingsPage.Update();
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            SettingsPage.Draw(sb, font);
+        }
+
+        public void Pause()
+        {
+
+        }
+
+        public void UnPause()
+        {
+
+        }
+
+        public void Update(IGameObject obj)
+        {
+        }
+    }
+}
---
+++ CrossPlatformDesktopProject/SettingsPage/ISettingsPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Sprint2
+{
+    public interface ISettingsPage
+    {
+        void selectOption();
+
+        void cycleOption();
+
+        void Update();
+
+        void Draw(SpriteBatch spriteBatch, SpriteFont spriteFont);
+    }
+}
