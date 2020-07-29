b702d410ad939d224d512247aaa651b7ce266a91
Terryl Rumph <trumph15bx@yahoo.com>
2020-07-28 23:53:33 -0400

    Added controls page. Doesnt do anything yet

--- CrossPlatformDesktopProject/SettingsPage/ControlsPage.cs
+++ CrossPlatformDesktopProject/SettingsPage/ControlsPage.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 namespace Sprint2
 {
     public class ControlsPage : AbstractSettingsPage
     {
         public ControlsPage()
         {
+            Title = "Controls Page";
+
+            Options.Add(new GamePad(new Vector2(SettingsConstants.Option1Position.X * Constant.DisplayScaleX, SettingsConstants.Option1Position.Y * Constant.DisplayScaleY), Color.White));
+
+            background = new Texture2D(Game1.Instance.GraphicsDevice, 1, 1);
+            background.SetData(new Color[] { Color.Black });
         }
     }
 }
