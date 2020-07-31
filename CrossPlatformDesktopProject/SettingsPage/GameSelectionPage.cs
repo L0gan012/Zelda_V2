b3384d385304fcbf5d1efb8dd5de0ddd1d68eee2
L0gan012 <logan.sauer.012@gmail.com>
2020-07-31 16:53:47 -0400

    Got second Dungeon xml added and started to option in the selection page

--- CrossPlatformDesktopProject/SettingsPage/GameSelectionPage.cs
+++ CrossPlatformDesktopProject/SettingsPage/GameSelectionPage.cs
@@ -7,7 +7,8 @@ namespace Sprint2
     {
         public GameSelectionPage()
         {
-            Options.Add(new GoToClassicGame(new Vector2(SettingsConstants.Option1Position.X * Constant.DisplayScaleX, SettingsConstants.Option1Position.Y * Constant.DisplayScaleY), Color.White));
+            Options.Add(new FirstDungeon(new Vector2(SettingsConstants.Option1Position.X * Constant.DisplayScaleX, SettingsConstants.Option1Position.Y * Constant.DisplayScaleY), Color.White));
+            Options.Add(new SecondDungeon(new Vector2(SettingsConstants.Option2Position.X * Constant.DisplayScaleX, SettingsConstants.Option2Position.Y * Constant.DisplayScaleY), Color.White));
             Options.Add(new GoToPreferences(new Vector2(SettingsConstants.Option2Position.X * Constant.DisplayScaleX, SettingsConstants.Option2Position.Y * Constant.DisplayScaleY), Color.White));
 
             background = new Texture2D(Game1.Instance.GraphicsDevice, 1, 1);
---
+++ CrossPlatformDesktopProject/SettingsPage/Option/FirstDungeon.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Sprint2
+{
+    public class FirstDungeon : AbstractOption
+    {
+        public FirstDungeon(Vector2 position, Color color)
+        {
+            Position = position;
+            Color = color;
+        }
+
+        public override void Select()
+        {
+            AddToPageStack();
+            Game1.Instance.State = new StateClassicGame();
+        }
+
+        public override void Draw(SpriteBatch spriteBatch, SpriteFont spriteFont)
+        {
+            spriteBatch.DrawString(spriteFont, "Dungeon - 1", Position, Color);
+        }
+    }
+}
