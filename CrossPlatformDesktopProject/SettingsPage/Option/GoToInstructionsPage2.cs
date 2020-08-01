7177bf79efff52a1ea2f0bb15fbea706ed2c9bf2
isabelsmithc <isabelsmithc@gmail.com>
2020-07-31 22:47:31 -0400

    added go to instructionspage 2

---
+++ CrossPlatformDesktopProject/SettingsPage/Option/GoToInstructionsPage2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Sprint2
+{
+    public class GoToInstructionsPage2 : AbstractOption
+    {
+        public GoToInstructionsPage2(Vector2 position, Color color)
+        {
+            Position = position;
+            Color = color;
+        }
+
+        public override void Select()
+        {
+            AddToPageStack();
+            if (Game1.Instance.State is StateSettings)
+            {
+                ((StateSettings)Game1.Instance.State).SettingsPage = new InstructionsPage2();
+            }
+        }
+
+        public override void Draw(SpriteBatch spriteBatch, SpriteFont spriteFont)
+        {
+            spriteBatch.DrawString(spriteFont, StringConstants.Page2, Position, Color);
+        }
+    }
+}
